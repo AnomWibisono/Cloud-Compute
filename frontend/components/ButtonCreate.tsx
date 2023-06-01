9a7f8755b77dc5074088f77aa49145902920d8d4
Kenneth Christopher Prasetya <kenneth.chr2003@mail.ugm.ac.id>
2023-06-01 20:43:21 +0700

    button

--- frontend/components/ButtonCreate.tsx
+++ frontend/components/ButtonCreate.tsx
@@ -1,11 +1,14 @@
+import Link from 'next/link';
+
 export default function ButtonCreate() {
-    return (
-      <>
+  return (
+    <>
+      <Link href="/createcontainer" passHref>
         <button className="bg-gray text-black font-semibold py-2 px-6 rounded-2xl font-body flex items-center drop-shadow text-xl">
           <img src="/add.svg" alt="Add" className="mr-2" />
           <span>Create</span>
         </button>
-      </>
-    );
-  }
-  
+      </Link>
+    </>
+  );
+}
--- frontend/src/pages/createcontainer.tsx
+++ frontend/src/pages/createcontainer.tsx
@@ -1,6 +1,12 @@
+import Navbar from '../../components/Navbar';
+
 export default function createcontainer(){
     return(
         <div>
+            <Navbar />
+            <div className='max-w-screen mx-auto pt-28 '>
+                <img src="./arrowback.svg" />
+            </div>
         </div>
     )
 }
